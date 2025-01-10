4768c8d8649faa9d3c4278cd588e9c81bd9bba7f
elagamy <https://github.com/elagamy257>
2025-01-10 22:31:20 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect } from 'react'
+import AOS from "aos"
+import "aos/dist/aos.css";
+import Navbar from './components/Navbar/Navbar';
+
+const App = () => {
+  useEffect(()=>{
+    AOS.init({
+      offset:100,
+      duration:700,
+      easing:"ease-in",
+      delay:100,
+    });
+  });
+  return(
+    <div className="overflow-x-hidden">
+      <Navbar />
+      
+      {/* <Home />
+      <About />
+      <Menu />
+      <Content /> */}
+
+    </div>
+  );
+  
+}
+
+export default App
